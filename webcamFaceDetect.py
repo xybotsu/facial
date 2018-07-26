78e5ab87164ed4d694328de4eb93184dbf1de4c2
albertywu@gmail.com <albertywu@gmail.com>
2018-07-26 16:38:05 -0700

    webcamFaceDetect.py

---
+++ webcamFaceDetect.py
@@ -0,0 +1,28 @@
+import cv2
+import sys
+
+faceCascade = cv2.CascadeClassifier(sys.argv[1])
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
